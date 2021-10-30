6caebbce020dae65eada79c67ac643bf12974527
Rian Mostert <rianmostert@hotmail.com>
2021-10-30 09:56:22 +0100

    Uncommented function code and fixed it up

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,43 +1,39 @@
-//
-// function dice1(){
+function dice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+var randomNumber1 = dice();
+var randomNumber2 = dice();
+
+document.querySelector('.img1').src = "images/dice" + randomNumber1 + ".png"
+document.querySelector('.img2').src = "images/dice" + randomNumber2 + ".png";
+
+function winner() {
+    if (randomNumber1 > randomNumber2) {
+        return "🚩Player 1 wins!";
+    } else if (randomNumber2 > randomNumber1) {
+        return "Player 2 wins!🚩";
+    } else if (randomNumber1 === randomNumber2) {
+        return "🚩DRAW🚩";
+    }
+}
+
+document.querySelector("h1").innerHTML = winner();
+
 //   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-//   return randomNumber1;
-// }
-// document.querySelector('.img1').src="images/dice" + dice1() + ".png";
 //
-// function dice2(){
+// document.querySelector('.img1').src="images/dice" + randomNumber1 + ".png";
+//
+//
 //   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-//   return randomNumber2;
-// }
-// document.querySelector('.img2').src="images/dice" + dice2() + ".png";
+//
+// document.querySelector('.img2').src="images/dice" + randomNumber2 + ".png";
 //
 //
-// function winner(){
-//   if (dice1() > dice2()){
-//     return "🚩Player 1 wins!";
-//   }else if(dice2() > dice1()){
-//     return "Player 2 wins!🚩";
-//   }else if (dice1() === dice2()){
-//     return "🚩DRAW🚩";
+//   if (randomNumber1 > randomNumber2){
+//     document.querySelector("h1").innerHTML = "🚩Player 1 wins";
+//   }else if(randomNumber1 < randomNumber2){
+//     document.querySelector("h1").innerHTML = "Player 2 wins🚩";
+//   }else if (randomNumber1 === randomNumber2){
+//     document.querySelector("h1").innerHTML = "🚩DRAW🚩";
 //   }
-// }
-//
-// document.querySelector("h1").innerHTML = winner();
-
-  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('.img1').src="images/dice" + randomNumber1 + ".png";
-
-
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('.img2').src="images/dice" + randomNumber2 + ".png";
-
-
-  if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩Player 1 wins";
-  }else if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 wins🚩";
-  }else if (randomNumber1 === randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩DRAW🚩";
-  }
